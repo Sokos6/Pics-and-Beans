f5f307836556e63fbd28e9de16cce312c24b7ee2
Sokos6 <will.sokolowski@gmail.com>
2020-10-01 12:06:28 -0500

    async for api call

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,15 @@ import axios from 'axios';
 import { ACCESS_KEY } from '../env';
 
 class App extends Component {
-  onSearchSubmit(term) {
-    axios
-      .get('https://api.unsplash.com/search/photos', {
-        params: { query: term },
-        headers: {
-          Authorization:
-            `Client-ID ${ACCESS_KEY}`,
-        },
-      })
-      .then((response) => {
-        console.log(response);
-      });
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: `Client-ID ${ACCESS_KEY}`,
+      },
+    });
   }
-
   render() {
-    // console.log(ACCESS_KEY);
     return (
       <div className='ui container' style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
