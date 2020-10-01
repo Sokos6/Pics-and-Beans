2b64e561b0438ab29137aa560317a0b43d1149d8
Sokos6 <will.sokolowski@gmail.com>
2020-10-01 11:31:43 -0500

    working api call

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import axios from 'axios';
+import { ACCESS_KEY } from '../env';
 
 class App extends Component {
-
   onSearchSubmit(term) {
-    console.log(term);
+    axios
+      .get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+          Authorization:
+            `Client-ID ${ACCESS_KEY}`,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+      });
   }
 
   render() {
+    // console.log(ACCESS_KEY);
     return (
       <div className='ui container' style={{ marginTop: '10px' }}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
