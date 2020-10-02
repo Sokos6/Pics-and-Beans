f185191fbb31c28953d68734bc31841516923b38
Sokos6 <will.sokolowski@gmail.com>
2020-10-02 14:45:56 -0500

    api client refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
-import axios from 'axios';
-import { ACCESS_KEY } from '../env';
+import unsplash from '../api/unsplash';
 
 class App extends Component {
-  async onSearchSubmit(term) {
-    const response = await axios.get('https://api.unsplash.com/search/photos', {
+  state = {
+    images: [],
+  };
+
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get('/search/photos', {
       params: { query: term },
-      headers: {
-        Authorization: `Client-ID ${ACCESS_KEY}`,
-      },
     });
-  }
+
+    this.setState({ images: response.data.results });
+  };
   render() {
     return (
       <div className='ui container' style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <div>Found: {this.state.images.length} Images</div>
       </div>
     );
   }
