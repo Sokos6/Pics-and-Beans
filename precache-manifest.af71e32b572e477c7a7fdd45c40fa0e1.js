a5f5c2ee335b0455d7148840bb4589a253dca58e
Sokos6 <will.sokolowski@gmail.com>
2020-10-12 19:14:10 -0500

    Updates

---
+++ precache-manifest.af71e32b572e477c7a7fdd45c40fa0e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b5bd2ccfcf6ac079e5ff867c6646df",
+    "url": "/Pics-and-Beans/index.html"
+  },
+  {
+    "revision": "9fdd2f2691116135a772",
+    "url": "/Pics-and-Beans/static/css/main.744c5823.chunk.css"
+  },
+  {
+    "revision": "cc3f4b9e23b8a017bade",
+    "url": "/Pics-and-Beans/static/js/2.d9bbd261.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pics-and-Beans/static/js/2.d9bbd261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fdd2f2691116135a772",
+    "url": "/Pics-and-Beans/static/js/main.c5e27712.chunk.js"
+  },
+  {
+    "revision": "43422ea6083c1fdea24a",
+    "url": "/Pics-and-Beans/static/js/runtime-main.1cae6ce9.js"
+  }
+]);
