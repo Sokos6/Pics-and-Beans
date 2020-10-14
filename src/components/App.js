f43980497781d614545be926d4723037129522b5
Sokos6 <will.sokolowski@gmail.com>
2020-10-14 18:27:34 -0500

    working with image tensorflow

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
 import unsplash from '../api/unsplash';
-
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import '@tensorflow/tfjs-backend-cpu';
 class App extends Component {
   state = {
     images: [],
@@ -14,6 +15,10 @@ class App extends Component {
     });
 
     this.setState({ images: response.data.results });
+    const img = this.state.image;
+    const model = await mobilenet.load();
+    const predictions = await model.classify(img);
+    console.log(predictions);
   };
   render() {
     return (
